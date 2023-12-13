a848196142cce286d6ade70db3a434ce17e7019d
Ballesteros-Romain <romain.ballesteros@gmail.com>
2023-12-14 00:29:28 +0100

    email in french and logout button

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_account'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
