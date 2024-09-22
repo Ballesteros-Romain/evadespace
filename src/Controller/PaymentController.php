61f2f5169cc3a3f755aeb4beb539c7a4acce8532
Ballesteros-Romain <romain.ballesteros@gmail.com>
2024-09-22 18:52:01 +0200

    ajout avec yoan

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -3,52 +3,77 @@
 namespace App\Controller;
 
 use Stripe\Stripe;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Entity\Workspace;
+use Stripe\Checkout\Session;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class PaymentController extends AbstractController
 {
-    #[Route('/order/create-session-stripe', name: 'app_payment')]
-//     // public function __construct(readonly private string $clientSecret)
-//     // {
-//     //     Stripe::setApiKey($this->clientSecret); 
-//     //     Stripe::setApiVersion('2023-10-16');
-//     // }
+    private EntityManagerInterface $entityManager;
 
-//     public function stripeChekout(): RedirectResponse
-//     {
-//         Stripe::setApiKey(sk_test_51ONPlRHBZr5XCfQnyvr7WqsePpV6X65XjiWe60wrz6H6GelZ2mwEnjF17jMX0F7ef49ybzfjjr9rCLpkCX3GcdI5001BJdYsgq);
-// // header('Content-Type: application/json');
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
-// // $YOUR_DOMAIN = 'http://localhost:4242';
+    #[Route('/payment', name: 'app_payment', methods: ['POST'])]
+    public function stripeChekout(Request $request): RedirectResponse
+    {
+        // TODO passer en env pour la sécurité
+        Stripe::setApiKey("sk_test_51ONPlRHBZr5XCfQnyvr7WqsePpV6X65XjiWe60wrz6H6GelZ2mwEnjF17jMX0F7ef49ybzfjjr9rCLpkCX3GcdI5001BJdYsgq");
+        Stripe::setApiVersion('2024-06-20');
+        
+        $workspace = $this->entityManager->getRepository(Workspace::class)->find((int)$request->request->get('workspaceId'));
+        // TODO Il faut stocker dans la session la date et heure choisie par le client/e (intercepté au clic sur Payer via le JavaScript)
 
-// $checkout_session = \Stripe\Checkout\Session::create([
-//   'line_items' => [[
-//     # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-//     'price' => '{{PRICE_ID}}',
-//     'quantity' => 1,
-//   ]],
-//   'mode' => 'payment',
-//   'success_url' => $YOUR_DOMAIN . '/success.html',
-//   'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
-// ]);
+        // TODO mettre l'URL dans le env plus tard
+        $url = 'https://localhost:8000';
 
-// header("HTTP/1.1 303 See Other");
-// header("Location: " . $checkout_session->url);
-//     }
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'line_items' => [[
+                // TODO ajouter une colonne stripe price ID sur l'entité Workspace pour stocker la valeur pour chaque plan
+                'price' => 'price_1Q1sYaHBZr5XCfQneqshWwR1',
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('payment_success'),
+            'cancel_url' => $this->generateUrl('payment_error'),
+        ]);
+            
+        header("HTTP/1.1 303 See Other");
+        header("Location: " . $checkout_session->url);
+    }
 
+    #[Route('/payment/success', name: 'payment_success')]
+    public function handleSuccessPayment(Request $request)
+    {
+        // TODO Ici, tu dois créer la réservation puisque le client a payé, donc tu valides
+        // Récupérer les données que tu as stocké en session afin de créer la réservation avec la date et heure choisie
 
-    // public function startPayment(){
+        return $this->render('payment/success.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
 
-    // }
+    #[Route('/payment/error', name: 'payment_error')]
+    public function handleErrorPayment(Request $request)
+    {
+        return $this->render('payment/error.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+
     #[Route('/tarif', name: 'app_tarif')]
     public function index(): Response
     {
         return $this->render('payment/index.html.twig', [
-          'controller_name' => 'PaymentController',
+            'controller_name' => 'PaymentController',
         ]);
-      }
+    }
 }
