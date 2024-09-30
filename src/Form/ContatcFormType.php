316077c8375578bdb7a7bf9a752982dd7e03a47b
Ballesteros-Romain <romain.ballesteros@gmail.com>
2024-09-30 10:55:05 +0200

    create formtype

---
+++ src/Form/ContatcFormType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use Assert\NotBlank;
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+class ContatcFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Commentaire',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le champ est obligatoire',
+                    ]),
+                    new Assert\NotBlank([
+                        'min' => 3,
+                        'max' => 500,
+                        'minMessage' => 'Le commentaire doit comporter au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le commentaire ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-Z0-9\s,\'"!?]+$/',
+                        'message' => 'Le commentaire contient des caractères non autorisés.',
+                    ]),
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
